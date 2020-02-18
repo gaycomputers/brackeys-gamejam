ab0931bb579f4e551b7b110c153b706e5214c332
josh <trisimix@protonmail.com>
2020-02-18 11:13:30 -0500

    gitignore and healthbar and stats

---
+++ brackeys-hole/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    public float maxHealth = 100;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.MaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (currentHealth == 0)
+        {
+            GameOver();
+        }
+    }
+
+    void Damage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GG");
+    }
+}
