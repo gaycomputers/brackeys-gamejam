0155a6395fd9a2ebbb3d6a7959a2199bb531f692
josh <trisimix@protonmail.com>
2020-02-20 18:53:18 -0500

    fix

--- brackeys-hole/Assets/Scripts/PlayerStats.cs
+++ brackeys-hole/Assets/Scripts/PlayerStats.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
-
     public float maxHealth = 100;
-    public float currentHealth;
+    public int startCurrency = 0;
+    public int startStrength = 1;
+    public float startSpeed = 5f;
+    public float actualMaxHealth;
+
+    public static int currentMaxHealth;
+    public static float currentSpeed;
+    public static int currentStrength;
+    public static float currentHealth;
+    public static int currentCurrency;
 
     public HealthBar healthBar;
 
     void Start()
     {
+        actualMaxHealth = maxHealth;
+        currentStrength = startStrength;
+        currentSpeed = startSpeed;
+        currentCurrency = startCurrency;
         currentHealth = maxHealth;
         healthBar.MaxHealth(maxHealth);
     }
@@ -24,7 +36,7 @@ public class PlayerStats : MonoBehaviour
         }
     }
 
-    void Damage(float damage)
+    void TakeDamage(float damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
