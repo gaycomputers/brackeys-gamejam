1048387d9384437522ade8a9d51b300545009b86
Hyegun <henrique.barigui@gmail.com>
2020-02-20 20:22:38 -0300

    try

---
+++ brackeys-hole/Assets/PlayerStats.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public float maxHealth = 100;
+    public int startCurrency = 0;
+    public int startStrength = 1;
+    public float startSpeed = 5f;
+    public float actualMaxHealth;
+
+    public static int currentMaxHealth;
+    public static float currentSpeed;
+    public static int currentStrength;
+    public static float currentHealth;
+    public static int currentCurrency;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        actualMaxHealth = maxHealth;
+        currentStrength = startStrength;
+        currentSpeed = startSpeed;
+        currentCurrency = startCurrency;
+        currentHealth = maxHealth;
+        healthBar.MaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (currentHealth == 0)
+        {
+            GameOver();
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GG");
+    }
+}
---
+++ brackeys-hole/Assets/Scripts/Shop.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+
+    private bool isActive = false;
+
+    void Update()
+    {
+        if (isActive == true)
+        {
+            OpenShop();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isActive == false)
+        {
+            isActive = true;
+        }
+        else
+        {
+            isActive = false;
+        }
+    }
+
+    public void OpenShop()
+    {
+
+    }
+
+    public void Heal()
+    {
+        if (PlayerStats.currentCurrency < 2)
+        {
+            return;
+        }
+        else
+        {
+            PlayerStats.currentCurrency -= 2;
+            PlayerStats.currentHealth += 50;
+        }
+    }
+
+    public void StrengthPlus()
+    {
+        if (PlayerStats.currentCurrency < 6)
+        {
+            return;
+        }
+        else
+        {
+            PlayerStats.currentCurrency -= 6;
+            PlayerStats.currentStrength += 1;
+        }
+    }
+
+    public void SpeedPlus()
+    {
+        if (PlayerStats.currentCurrency < 5)
+        {
+            return;
+        }
+        else
+        {
+            PlayerStats.currentCurrency -= 5;
+            PlayerStats.currentSpeed += 2f;
+        }
+    }
+
+    public void HealthPlus()
+    {
+        if (PlayerStats.currentCurrency < 6)
+        {
+            return;
+        }
+        else
+        {
+            PlayerStats.currentCurrency -= 6;
+            PlayerStats.currentMaxHealth += 50;
+        }
+    }
+}
+
+
