a72bd960cdea90f39d996b4995131cba1885d8cc
josh <trisimix@protonmail.com>
2020-02-20 21:37:21 -0500

    ok

---
+++ brackeys-hole/Assets/Scripts/CenterRoomScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterRoomScript : MonoBehaviour
+{
+
+    public GameObject cover;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cover, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+}
---
+++ brackeys-hole/Assets/Scripts/CoverScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoverScript : MonoBehaviour
+{
+    SpriteRenderer rend;
+    private bool unfaded = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = .8f;
+        rend.material.color = c;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.tag == "Player" && unfaded){
+            unfaded = false;
+            StartCoroutine("FadeOut");
+        }
+    }
+    IEnumerator FadeOut(){
+        for (float f = 1f; f >= -0.3; f -= 0.3f){
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+        
+    }
+}
